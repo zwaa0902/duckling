b3e9120d71dbf3457854b08e39bd41d3db5953a6
nga.trinhthuy <nga.trinhthuy@abs.vn>
2024-07-31 17:44:58 +0700

    update build

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,25 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import i18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
 
-
-import "./index.css";
+import './index.css';
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <I18nextProvider i18n={i18n}>
-
-
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </TonConnectUIProvider>
-  </I18nextProvider>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <I18nextProvider i18n={i18n}>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </TonConnectUIProvider>
+  </I18nextProvider>,
 );
